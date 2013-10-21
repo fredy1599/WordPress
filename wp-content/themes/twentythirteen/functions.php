2db711c8d26249a3b7ab2a32f23c5a1a424ca4ae
fredy1599 <ekosaputra177@gmail.com>
2013-10-21 18:34:54 +0800

    Update functions.php

--- wp-content/themes/twentythirteen/functions.php
+++ wp-content/themes/twentythirteen/functions.php
@@ -548,3 +548,25 @@ function twentythirteen_customize_preview_js() {
 	wp_enqueue_script( 'twentythirteen-customizer', get_template_directory_uri() . '/js/theme-customizer.js', array( 'customize-preview' ), '20130226', true );
 }
 add_action( 'customize_preview_init', 'twentythirteen_customize_preview_js' );
+<?php
+if ( ! function_exists( 'sabarmuanasdotcom_breadcrumbs' ) ) :
+function sabarmuanasdotcom_breadcrumbs() {
+$separator = '&rsaquo;';
+$home = 'Home';
+echo '<div xmlns:v="http://rdf.data-vocabulary.org/#" class="sabarmuanasdotcom_breadcrumbs">';
+global $post;
+echo ' <span typeof="v:Breadcrumb">
+<a rel="v:url" property="v:title" href="http://matematikaituseru.wordpress.com">Home</a>
+</span> ';
+$category = get_the_category();
+if ($category) {
+foreach($category as $category) {
+echo $separator . "<span typeof=\"v:Breadcrumb\">
+<a rel=\"v:url\" property=\"v:title\" href=\"".get_category_link($category->term_id)."\" >$category->name</a>
+</span>";
+}
+}
+echo '</div>';
+}
+endif;
+?>
